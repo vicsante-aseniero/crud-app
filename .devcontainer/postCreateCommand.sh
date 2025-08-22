cc4fbe49a4da7812dc6a1bc65c81350d9cc26e33
Vicsante Aseniero <vaseniero@gmail.com>
2025-08-23 06:00:34 +0800

    Fix Configuration files and folder structure.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,12 @@
-#script.sh
-
 #!/bin/sh
-go version
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y xdg-utils
+sudo apt-get update && sudo apt-get install -y net-tools
+
+echo "Installing needed package(s)....."
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go version
 protoc --version
 dapr version
 echo "Done version verifications and installing needed package(s)....."
