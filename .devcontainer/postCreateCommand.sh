b64d5ac22b1d79b87425288fe5134877dfeaff47
Vicsante Aseniero <vaseniero@gmail.com>
2025-09-26 16:01:26 +0800

    Updated configurations.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y xdg-utils
 sudo apt-get update && sudo apt-get install -y net-tools
@@ -7,9 +8,17 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 
 echo "Installing needed package(s)....."
+
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 go version
+node --version
+pnpm --version
+npm --version
+yarn --version
 protoc --version
 dapr version
+protoc --version
+
 echo "Done version verifications and installing needed package(s)....."
